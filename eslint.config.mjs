cb26f8f7965198f6066ee59eeebd7f13f2d25781
Jackson Ming Hu <huming2207@gmail.com>
2024-08-20 22:40:53 +1000

    Rerun linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,14 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"],},
-  {languageOptions: { globals: globals.node, ecmaVersion: 5, sourceType: "module" }, rules: {
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: { globals: globals.node, ecmaVersion: 5, sourceType: "module" },
+    rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "no-useless-escape": "off"
-  }, },
+      "no-useless-escape": "off",
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettier,
