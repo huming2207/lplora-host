09f7558a10b13de5667b2a3221b34ed0af79defe
Jackson Ming Hu <huming2207@gmail.com>
2024-09-17 11:10:33 +1000

    Add battery testing code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
+import { LoRaBandwidth, LoRaCodingRate, LoRaSpreadingFactor } from "./constant";
 import { LpLoraDriver } from "./driver";
-import { UartRadioFreqCfgPacket, UartRadioLoRaCfgPacket, UartRadioPhyCfgPacket, UartRadioTxPacket } from "./packet";
+import { UartRadioFreqCfgPacket, UartRadioLoRaCfgPacket, UartRadioPhyCfgPacket, UartRadioRxPacket } from "./packet";
+import fs from "fs";
 
-const port = process.env["LPLORA_PORT"] || "/dev/tty.wchusbserial31440";
+const port = process.env["LPLORA_PORT"] || "/dev/tty.usbmodem314402";
+// const port = process.env["LPLORA_PORT"] || "/dev/tty.wchusbserial3110";
 
 const device = new LpLoraDriver(port);
-await device.sendPing();
-
-await device.sendPing();
-await device.sendPing();
-await device.sendPing();
-
-await device.sendPing();
-
 await device.sendPacket(new UartRadioPhyCfgPacket());
 
 const loraCfg = new UartRadioLoRaCfgPacket();
-loraCfg.payloadLen = 10;
-loraCfg.preambleLen = 10;
+loraCfg.payloadLen = 24;
+loraCfg.preambleLen = 32;
+loraCfg.enableCRC = true;
+loraCfg.invertIQ = false;
+loraCfg.fixedHeader = false;
+loraCfg.spreadFactor = LoRaSpreadingFactor.Sf12;
+loraCfg.codingRate = LoRaCodingRate.Cr45;
+loraCfg.bandwidth = LoRaBandwidth.Bw125;
 await device.sendPacket(loraCfg);
 
 const freqCfg = new UartRadioFreqCfgPacket();
-freqCfg.frequencyHz = 919000000;
+freqCfg.frequencyHz = 926000000;
 await device.sendPacket(freqCfg);
 
 setInterval(async () => {
-  const txReq = new UartRadioTxPacket();
-  txReq.payload = Buffer.from([0xca, 0xfe, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef]);
-  await device.sendPacket(txReq);
-}, 500);
+  const rxReq = new UartRadioRxPacket();
+  await device.sendPacket(rxReq);
+}, 11000);
 
 device.on("rawDataReceived", (data) => {
   console.log(`Raw: ${data.toString("hex")}`);
@@ -35,4 +35,7 @@ device.on("rawDataReceived", (data) => {
 
 device.on("packetReceived", (data) => {
   console.log(`Packet: ${JSON.stringify(data)}`);
+  fs.appendFile("packet.log", `${new Date().toString()},${JSON.stringify(data)}`, (err) => {
+    console.error(err);
+  });
 });
