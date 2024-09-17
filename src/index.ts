b25277fd162983848b59ee34dceb360896f3853c
Jackson Ming Hu <huming2207@gmail.com>
2024-09-17 23:16:55 +1000

    Make it works

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,13 @@
-import { LoRaBandwidth, LoRaCodingRate, LoRaSpreadingFactor } from "./constant";
 import { LpLoraDriver } from "./driver";
-import {
-  UartPingPacket,
-  UartRadioFreqCfgPacket,
-  UartRadioLoRaCfgPacket,
-  UartRadioPhyCfgPacket,
-  UartRadioRxPacket,
-} from "./packet";
+import { UartRadioReceivedPacket, UartRadioRxPacket } from "./packet";
 import fs from "fs";
 
 const port = process.env["LPLORA_PORT"] || "/dev/tty.usbmodem314402";
 // const port = process.env["LPLORA_PORT"] || "/dev/tty.wchusbserial3110";
 
 const device = new LpLoraDriver(port);
-await device.sendPacket(new UartRadioPhyCfgPacket());
-
-const ping = new UartPingPacket();
-device.sendPacket(ping);
-
-const loraCfg = new UartRadioLoRaCfgPacket();
-loraCfg.payloadLen = 24;
-loraCfg.preambleLen = 8;
-loraCfg.enableCRC = true;
-loraCfg.invertIQ = false;
-loraCfg.fixedHeader = false;
-loraCfg.spreadFactor = LoRaSpreadingFactor.Sf12;
-loraCfg.codingRate = LoRaCodingRate.Cr45;
-loraCfg.bandwidth = LoRaBandwidth.Bw125;
-loraCfg.syncWord = Buffer.from([0x24, 0x34]);
-await device.sendPacket(loraCfg);
-
-const freqCfg = new UartRadioFreqCfgPacket();
-freqCfg.frequencyHz = 926000000;
-await device.sendPacket(freqCfg);
-
-device.sendPacket(ping);
 
 setInterval(async () => {
-  await device.sendPacket(loraCfg);
-  await device.sendPacket(freqCfg);
   const rxReq = new UartRadioRxPacket();
   rxReq.timeoutMillisec = 350000;
   await device.sendPacket(rxReq);
@@ -50,7 +19,11 @@ device.on("rawDataReceived", (data) => {
 
 device.on("packetReceived", (data) => {
   console.log(`Packet: ${JSON.stringify(data)}`);
-  fs.appendFile("packet.log", `${new Date().toString()},${JSON.stringify(data)}\r\n`, (err) => {
-    console.error(err);
-  });
+  const packet = data as UartRadioReceivedPacket;
+  if (Buffer.isBuffer(packet.receivedData)) {
+    const counter = packet.receivedData.readUInt32LE(packet.receivedData.length - 4);
+    fs.appendFile("packet.log", `'${new Date().getTime()}','${counter}','${JSON.stringify(data)}'\r\n`, (err) => {
+      console.error(err);
+    });
+  }
 });
